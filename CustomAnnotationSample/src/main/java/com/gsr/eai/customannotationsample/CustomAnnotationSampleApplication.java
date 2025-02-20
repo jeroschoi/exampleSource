1cb06aa992b8797e157c924786bf63df3dba44e0
suwon82 <suwon82@gsretail.com>
2025-02-20 17:24:42 +0900

    최초 commit

---
+++ CustomAnnotationSample/src/main/java/com/gsr/eai/customannotationsample/CustomAnnotationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.gsr.eai.customannotationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnnotationSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomAnnotationSampleApplication.class, args);
+    }
+
+}
