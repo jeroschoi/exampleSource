21313fff5eef0ccb62636cef8f61aea5e2e7ee77
suwon82 <suwon82@gsretail.com>
2025-02-28 09:42:17 +0900

    Scheduling Annotation 추가

--- CustomAnnotationSample/src/main/java/com/sample/customannotationsample/CustomAnnotationSampleApplication.java
+++ CustomAnnotationSample/src/main/java/com/sample/customannotationsample/CustomAnnotationSampleApplication.java
@@ -2,7 +2,9 @@ package com.sample.customannotationsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CustomAnnotationSampleApplication {
     public static void main(String[] args) {
